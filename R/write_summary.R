30628c8f2b8819b676d48bbd776ad0cbcc45966f
Jonghyun Yun <jonghyun.yun@gmail.com>
2021-01-08 04:31:27 -0600

    chessB: w0 and z0

--- R/write_summary.R
+++ R/write_summary.R
@@ -7,3 +7,11 @@ readr::write_csv(dout, paste0(out_dir, "beta_theta_mean.csv"))
 rr = grep("^lambda",  names(mm))
 dout = data.frame(vname = names(mm[rr]), mean = mm[rr])
 readr::write_csv(dout, paste0(out_dir, "lambda_mean.csv"))
+
+rr = grep("^^z\\.0\\.",  names(mm))
+dout = data.frame(vname = names(mm[rr]), mean = mm[rr])
+readr::write_csv(dout, paste0(out_dir, "z0_mean.csv"))
+
+rr = grep("^^z\\.0\\.",  names(mm))
+dout = data.frame(vname = names(mm[rr]), mean = mm[rr])
+readr::write_csv(dout, paste0(out_dir, "z0_mean.csv"))
