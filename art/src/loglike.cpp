ee19d25f73626a59943331ed8b0c3c2333edd78a
Jonghyun Yun <jonghyun.yun@gmail.com>
2021-04-05 18:48:59 -0500

    0.1.2

--- art/src/loglike.cpp
+++ art/src/loglike.cpp
@@ -8,25 +8,26 @@ using MapVecd = Eigen::Map<Eigen::VectorXd>;
 // typedef Eigen::Map<Eigen::MatrixXi> MapMati; // cannot map integermatrix??
 using MapVeci = Eigen::Map<Eigen::VectorXi>;
 
-using Rcpp::NumericVector;
-using Rcpp::NumericMatrix;
+using Rcpp::as;
 using Rcpp::IntegerMatrix;
 using Rcpp::IntegerVector;
 using Rcpp::List;
-using Rcpp::as;
+using Rcpp::NumericMatrix;
+using Rcpp::NumericVector;
 
 using Eigen::MatrixXd;
-using Eigen::VectorXd;
 using Eigen::MatrixXi;
+using Eigen::VectorXd;
 using Eigen::VectorXi;
 
 // find i such that x is in i-th interval (for i=1,...,(# breaks - 1))
 // x below the lower bound gives i = 0
 // x above the upper bound gives i = # of breaks
-int findInterval(const double x, const VectorXd& breaks) noexcept {
+int findInterval(const double x, const VectorXd &breaks) noexcept {
   int out = 0;
   for (int i = 0; i < breaks.size(); i++) {
-    if (x <= breaks(i)) break;
+    if (x <= breaks(i))
+      break;
     ++out;
   }
   return out;
@@ -67,6 +68,7 @@ MatrixXd reshape_rowmajor(NumericMatrix::Row &row, int nrow, int ncol) {
   return res;
 }
 
+// 2D l2-norm
 double distance(NumericMatrix::Row z_k, NumericMatrix::Row w_i) {
   double dist = 0;
   for (int d = 0; d < 2; d++) {
@@ -94,16 +96,13 @@ private:
   MatrixXd w;
 
 public:
-
   samples(NumericMatrix::Row lambda_, NumericMatrix::Row theta_,
           NumericMatrix::Row z_, NumericMatrix::Row w_,
-          NumericMatrix::Row gamma_, Rcpp::List param_):
-    //   : I(as<int>(param_["I"])), N(as<int>(param_["N"])),
-    //     G(as<int>(param_["G"])) {
-        sj(as<MapVecd>(param_["sj"])),
-        H(as<MapMatd>(param_["H"])),
-        len(as<MapVeci>(param_["len"]))
-    {
+          NumericMatrix::Row gamma_, Rcpp::List param_)
+      : //   : I(as<int>(param_["I"])), N(as<int>(param_["N"])),
+        //     G(as<int>(param_["G"])) {
+        sj(as<MapVecd>(param_["sj"])), H(as<MapMatd>(param_["H"])),
+        len(as<MapVeci>(param_["len"])) {
 
     MatrixXd tmp_seg = as<MapMatd>(param_["seg"]);
     MatrixXd tmp_Y = as<MapMatd>(param_["Y"]);
@@ -126,82 +125,80 @@ public:
     theta = reshape_rowmajor(theta_, N, 2);
     z = reshape_rowmajor(z_, 2 * N, 2);
     w = reshape_rowmajor(w_, 2 * I, 2);
+  }
+
+  void set_seg(int i, const VectorXi &seg_i) {
+    try {
+      if (seg_i.size() != N) throw 0;
     }
+    catch (int n) {std::cout << "size mismatched" << std::endl;}
+    seg.row(i) = seg_i;
+  }
 
-    void set_seg(int i, const VectorXi& seg_i) {
-      try {
-        if (seg_i.size() != N) throw 0;
-      }
-      catch (int n) {std::cout << "size mismatched" << std::endl;}
-      seg.row(i) = seg_i;
-    }
+  void eval_cum_lambda() noexcept {
+    cum_lambda.setZero(2 * I, N); // matrx(0, 2 * I, N)
 
-    void eval_cum_lambda() {
-      cum_lambda.setZero(2 * I, N); // matrx(0, 2 * I, N)
-
-      for (int c = 0; c < 2; c++) {
-        for (int i = 0; i < I; i++) {
-          for (int k = 0; k < N; k++) {
-            // if (NA(i, k) == 1) {
-            for (int g = 0; g < seg(i, k); g++) {
-              // std::cout << seg(i,k) << std::endl;
-              cum_lambda(c * I + i, k) += len(g) * lambda(c * I + i, g);
-            }
-            cum_lambda(c * I + i, k) += H(i, k) * lambda(c * I + i, seg(i, k));
-            // }
+    for (int c = 0; c < 2; c++) {
+      for (int i = 0; i < I; i++) {
+        for (int k = 0; k < N; k++) {
+          // if (NA(i, k) == 1) {
+          for (int g = 0; g < seg(i, k); g++) {
+            // std::cout << seg(i,k) << std::endl;
+            cum_lambda(c * I + i, k) += len(g) * lambda(c * I + i, g);
           }
+          cum_lambda(c * I + i, k) += H(i, k) * lambda(c * I + i, seg(i, k));
+          // }
         }
       }
     }
+  }
 
+  double log_rr(int c, int i, int k) {
+    return theta(k, c) -
+           gamma(c) * (z.row(c * N + k) - w.row(c * I + i)).norm();
+  }
 
-    double log_rr(int c, int i, int k) {
-      return theta(k, c) - gamma(c) * (z.row(c * N + k) - w.row(c * I + i)).norm();
-    }
+  // log(lambda) + log_rr
+  double log_hazard(int c, int i, int k) {
+    return std::log(lambda(c * I + i, seg(i, k))) + log_rr(c, i, k);
+  }
 
-    // log(lambda) + log_rr
-    double log_hazard(int c, int i, int k) {
-      return std::log(lambda(c * I + i, seg(i, k))) + log_rr(c,i,k);
-    }
+  double hazard(int c, int i, int k) { return std::exp(log_hazard(c, i, k)); }
 
-    double hazard(int c, int i, int k) {
-      return std::exp(log_hazard(c,i,k));
-    }
+  double eval_acc(int i, int k) {
+    return hazard(1, i, k) / (hazard(1, i, k) + hazard(0, i, k));
+  }
 
-    double eval_acc(int i, int k) {
-      return hazard(1,i,k) / (hazard(1,i,k) + hazard(0,i,k));
-    }
+  // void print_param(int i, int k) {
+  //   for (int c = 0; c < 2; c++) {
+  //   std::cout << seg(i,k) << "," << lambda(c * I + i,seg(i,k)) << "," <<
+  //   theta(k,c) << "," << gamma(c) << "," << (z.row(c * N + k) - w.row(c * I +
+  //   i)).norm() << "," << hazard(c,i,k) << std::endl; std::cout <<
+  //   "====================================\n";
+  //   }
+  //   std::cout << eval_acc(i,k) << std::endl;;
+  // }
 
-    // void print_param(int i, int k) {
-    //   for (int c = 0; c < 2; c++) {
-    //   std::cout << seg(i,k) << "," << lambda(c * I + i,seg(i,k)) << "," << theta(k,c) << "," << gamma(c) << "," << (z.row(c * N + k) - w.row(c * I + i)).norm() << "," << hazard(c,i,k) << std::endl;
-    //   std::cout << "====================================\n";
-    //   }
-    //   std::cout << eval_acc(i,k) << std::endl;;
-    // }
-
-    VectorXd acc_item(int item) {
-      VectorXd acc(N);
-      for (int k = 0; k < N; k++) {
+  VectorXd acc_item(int item) {
+    VectorXd acc(N);
+    for (int k = 0; k < N; k++) {
       acc(k) = eval_acc(item, k);
-      }
-      return acc;
     }
+    return acc;
+  }
 
   double loglike() {
     eval_cum_lambda();
     double running_total = 0;
-  //   // std::cout << "Calculating the log-acceptance ratio...\n";
+    //   // std::cout << "Calculating the log-acceptance ratio...\n";
     for (int c = 0; c < 2; c++) {
       for (int i = 0; i < I; i++) {
         for (int k = 0; k < N; k++) {
 
-          running_total -=
-              cum_lambda(c * I + i, k) *
-              std::exp(log_rr(c,i,k));
+          running_total -= cum_lambda(c * I + i, k) * std::exp(log_rr(c, i, k));
 
           if (Y(i, k) == c) {
-            running_total += log_hazard(c,i,k);
+            running_total += log_hazard(c, i, k);
           }
         }
       }
@@ -214,11 +211,11 @@ public:
     VectorXd haz = VectorXd::Zero(G);
     VectorXd hazlen = VectorXd::Zero(G);
 
-      for (int g = 0; g < G; g++) {
-    for (int c = 0; c < 2; c++) {
+    for (int g = 0; g < G; g++) {
+      for (int c = 0; c < 2; c++) {
         haz(g) += lambda(c * I + i, g) * std::exp(log_rr(c, i, k));
       }
-    hazlen(g) = haz(g) * len(g);
+      hazlen(g) = haz(g) * len(g);
     }
 
     VectorXd cumhaz = my_cumsum(hazlen);
@@ -233,29 +230,29 @@ public:
     //     throw 0;
     // } catch (int n) {
     //   std::cout << "Caught " << n << std::endl;
-    // if (k == 0) {
-//       std::cout << "i, k: " << i << "," << k << std::endl;
-//       for (int c = 0; c < 2; c++) {
-// std::cout <<
-//       theta(k, c) << "," << gamma(c) << "," << (z.row(c * N + k) - w.row(c * I + i)).norm() << std::endl;
+    //       std::cout << "i, k: " << i << "," << k << std::endl;
+    //       for (int c = 0; c < 2; c++) {
+    // std::cout <<
+    //       theta(k, c) << "," << gamma(c) << "," << (z.row(c * N + k) -
+    //       w.row(c * I + i)).norm() << std::endl;
 
-// std::cout << z.row(c * N + k) << "," << w.row(c * I + i)<< std::endl;
-//         for (int g = 0; g < G; g++) {
+    // std::cout << z.row(c * N + k) << "," << w.row(c * I + i)<< std::endl;
+    //         for (int g = 0; g < G; g++) {
 
-//           std::cout << "c, g: " << c << "," << g << std::endl;
-//           std::cout << "lambda(c * I + i, g)" << lambda(c * I + i, g)
-//                     << std::endl;
-//           std::cout << "rr" << std::exp(log_rr(c, i, k)) << std::endl;
-//           std::cout << "len" << len(g) << std::endl;
-//         }
-//       }
+    //           std::cout << "c, g: " << c << "," << g << std::endl;
+    //           std::cout << "lambda(c * I + i, g)" << lambda(c * I + i, g)
+    //                     << std::endl;
+    //           std::cout << "rr" << std::exp(log_rr(c, i, k)) << std::endl;
+    //           std::cout << "len" << len(g) << std::endl;
+    //         }
+    //       }
 
-      // std::cout << "cumhaz" << std::endl;
-      // std::cout << cumhaz << std::endl;
-      // std::cout << "-1.0 * logS" << std::endl;
-      // std::cout << -1.0 * logS << std::endl;
-      // std::cout << "ss" << std::endl;
-      // std::cout << ss << std::endl;
+    // std::cout << "cumhaz" << std::endl;
+    // std::cout << cumhaz << std::endl;
+    // std::cout << "-1.0 * logS" << std::endl;
+    // std::cout << -1.0 * logS << std::endl;
+    // std::cout << "ss" << std::endl;
+    // std::cout << ss << std::endl;
 
     // }
 
@@ -266,7 +263,7 @@ public:
     return vtime;
   }
 
-  VectorXd gen_vtime(int item) {
+  VectorXd gen_vtime(int item) noexcept {
     VectorXd res(N);
     for (int k = 0; k < N; k++) {
       res(k) = gen_time(item, k);
@@ -275,7 +272,7 @@ public:
   }
 
   double logprior() { return 0; }
-  };
+};
 
 // [[Rcpp::export]]
 NumericVector get_loglike(NumericMatrix lambda_, NumericMatrix theta_,
@@ -298,14 +295,20 @@ NumericVector get_loglike(NumericMatrix lambda_, NumericMatrix theta_,
 // TODO: create member functions
 // [[Rcpp::export]]
 Eigen::MatrixXd rcpp_gen_surv_pp(NumericMatrix lambda_, NumericMatrix theta_,
-                            NumericMatrix z_, NumericMatrix w_,
-                            NumericMatrix gamma_, List &param_, int item) {
+                                 NumericMatrix z_, NumericMatrix w_,
+                                 NumericMatrix gamma_, List &param_, int item) {
 
   int num_iter = lambda_.nrow();
   int N = theta_.ncol() / 2;
-  // int I = w_.nrow() / 2;
+  int I = w_.nrow() / 2;
 
-  // if (item < 0 || item > (I-1)) return 0;
+  try {
+    if (item < 1 || item > I)
+      throw 0;
+  } catch (int n) {
+    std::cout << "item must be between 1 and I." << std::endl;
+  }
+  item -= 1; // R to C indexing
 
   Eigen::MatrixXd res(num_iter, N);
 
@@ -314,7 +317,7 @@ Eigen::MatrixXd rcpp_gen_surv_pp(NumericMatrix lambda_, NumericMatrix theta_,
   for (int nn = 0; nn < num_iter; nn++) {
     samples sample_l(lambda_.row(nn), theta_.row(nn), z_.row(nn), w_.row(nn),
                      gamma_.row(nn), param_);
-    res.row(nn) = sample_l.acc_item(item-1); // indexing adjust
+    res.row(nn) = sample_l.acc_item(item);
     Rcpp::checkUserInterrupt();
   }
   // int nn = 0;
@@ -326,14 +329,21 @@ Eigen::MatrixXd rcpp_gen_surv_pp(NumericMatrix lambda_, NumericMatrix theta_,
 
 // [[Rcpp::export]]
 Eigen::MatrixXd rcpp_gen_surv_time(NumericMatrix lambda_, NumericMatrix theta_,
-                            NumericMatrix z_, NumericMatrix w_,
-                            NumericMatrix gamma_, List &param_, int item) {
+                                   NumericMatrix z_, NumericMatrix w_,
+                                   NumericMatrix gamma_, List &param_,
+                                   int item) {
 
   int num_iter = lambda_.nrow();
   int N = theta_.ncol() / 2;
-  // int I = w_.nrow() / 2;
+  int I = w_.nrow() / 2;
 
-  // if (item < 0 || item > (I-1)) return 0;
+  try {
+    if (item < 1 || item > I)
+      throw 0;
+  } catch (int n) {
+    std::cout << "item must be between 1 and I." << std::endl;
+  }
+  item -= 1; // R to C indexing
 
   Eigen::MatrixXd res(num_iter, N);
 
@@ -342,7 +352,7 @@ Eigen::MatrixXd rcpp_gen_surv_time(NumericMatrix lambda_, NumericMatrix theta_,
   for (int nn = 0; nn < num_iter; nn++) {
     samples sample_l(lambda_.row(nn), theta_.row(nn), z_.row(nn), w_.row(nn),
                      gamma_.row(nn), param_);
-    res.row(nn) = sample_l.gen_vtime(item-1); // indexing adjust
+    res.row(nn) = sample_l.gen_vtime(item);
     Rcpp::checkUserInterrupt();
   }
   // int nn = 0;
@@ -352,27 +362,37 @@ Eigen::MatrixXd rcpp_gen_surv_time(NumericMatrix lambda_, NumericMatrix theta_,
   return res;
 }
 
+// calculate segment for new time values
 template <typename T>
 VectorXi time_seg(const T &x, const VectorXd &sj) noexcept {
   VectorXi out(x.size());
 
-    for (int i = 0; i < x.size(); i++) {
+  for (int i = 0; i < x.size(); i++) {
     out(i) = findInterval(x(i), sj) - 1;
-    if (out(i) == (sj.size() - 1)) out(i) -= 1;
+    if (out(i) == (sj.size() - 1))
+      out(i) -= 1; // out of range
   }
-
   return out;
 }
 
 // [[Rcpp::export]]
 Rcpp::List rcpp_gen_surv(NumericMatrix lambda_, NumericMatrix theta_,
-                            NumericMatrix z_, NumericMatrix w_,
-                            NumericMatrix gamma_, List &param_, int item) {
+                         NumericMatrix z_, NumericMatrix w_,
+                         NumericMatrix gamma_, List &param_, int item) {
 
   int num_iter = lambda_.nrow();
   int N = theta_.ncol() / 2;
+  int I = w_.nrow() / 2;
   // int I = w_.nrow() / 2;
   VectorXd sj(as<MapVecd>(param_["sj"]));
+
+  try {
+    if (item < 1 || item > I)
+      throw 0;
+  } catch (int n) {
+    std::cout << "item must be between 1 and I." << std::endl;
+  }
+  item -= 1; // R to C indexing
 
   // if (item < 0 || item > (I-1)) return 0;
 
@@ -381,21 +401,15 @@ Rcpp::List rcpp_gen_surv(NumericMatrix lambda_, NumericMatrix theta_,
 
   // std::cout << "res: " << res.at(1) << std::endl;
   for (int nn = 0; nn < num_iter; nn++) {
-  // for (int nn = 0; nn < 1; nn++) {
+    // for (int nn = 0; nn < 1; nn++) {
     samples sample_l(lambda_.row(nn), theta_.row(nn), z_.row(nn), w_.row(nn),
                      gamma_.row(nn), param_);
-    res_t.row(nn) = sample_l.gen_vtime(item-1); // indexing adjust
+    res_t.row(nn) = sample_l.gen_vtime(item); // indexing adjust
     sample_l.set_seg(item, time_seg(res_t.row(nn), sj));
-    res_p.row(nn) = sample_l.acc_item(item-1); // indexing adjust
+    res_p.row(nn) = sample_l.acc_item(item); // indexing adjust
     Rcpp::checkUserInterrupt();
   }
-  // int nn = 0;
-  // samples sample_l(lambda_.row(nn), theta_.row(nn), z_.row(nn), w_.row(nn),
-  //                  gamma_.row(nn), param_);
-  // sample_l.print_param(39, 0);
 
-    return Rcpp::List::create(
-        Rcpp::Named("time") = res_t,
-        Rcpp::Named("pp") = res_p
-    );
+  return Rcpp::List::create(Rcpp::Named("time") = res_t,
+                            Rcpp::Named("pp") = res_p);
 }
