13eeb02c6f2c497f6e6586b941ee135ab4862e79
Jonghyun Yun <jonghyun.yun@gmail.com>
2021-04-08 16:43:12 -0500

    pushing changes 2021-04-08 16:43:12 CDT

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((TeX-engine . xetex)
-         (eval . (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]art\\'")))
+((nil . ((TeX-engine . xetex))
          ))
